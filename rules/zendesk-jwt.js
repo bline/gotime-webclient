afbc7dddc55776f770d2906d41a1473d6e76b301
bline <scottbeck@gmail.com>
2018-02-17 04:04:14 -0700

    testing zendesk integration

---
+++ rules/zendesk-jwt.js
@@ -0,0 +1,21 @@
+function (user, context, callback) {
+
+  // Generate a random UUID: http://en.wikipedia.org/wiki/Universally_unique_identifier
+  // Used in the token's jti claim to protect against replay attacks
+  var uuid = require('uuid');
+
+  // Create the JWT as required by Zendesk
+  var payload = {
+    iat: new Date().getTime() / 1000,
+    jti: uuid.v4(),
+    email: user.email,
+    name: user.name,
+    external_id: user.user_id
+  };
+
+  // Sign the token and add it to the profile
+  var zendesk_token = jwt.sign(payload, configuration.ZENDESK_JWT_SECRET);
+  user.zendesk_jwt_url = 'https://' + configuration.ZENDESK_SUBDOMAIN + '.zendesk.com/access/jwt?jwt=' + zendesk_token;
+
+  callback(null, user, context);
+}
